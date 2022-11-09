4b03f60ba59715084aa4ea63848b9e179021b71f
aceal072161 <116267421+aceal072161@users.noreply.github.com>
2022-11-09 21:34:26 +0800

    Add files via upload

---
+++ reversed-triangle/reverse-triangle.js
@@ -0,0 +1,12 @@
+//reversed triangle
+var s = "";
+for (var i = 0; i < 10; i++) {
+  for (var h = i; h < 10; h++) {
+    s += "* ";
+  }
+  s += "\n";
+}
+console.log(s);
+
+//https://www.796t.com/article.php?id=216199
+//再去想
---
+++ triangle/triangle.js
@@ -0,0 +1,9 @@
+let s = "";
+for (let i = 0; i < 10; i++) {
+  for (let h = 0; h < i; h++) {
+    s += "* ";
+  }
+  s += "\n";
+}
+console.log(s);
+//再去想
